626c6af177f41316072188dede483cdd469eca0d
Alex Carter <victoryforphil@gmail.com>
2024-08-14 23:35:39 -0700

    Some Database tests and utils

--- datastore/src/database/mod.rs
+++ datastore/src/database/mod.rs
@@ -1,13 +1,21 @@
-use std::collections::BTreeMap;
-
 use crate::{
     buckets::{Bucket, BucketHandle},
+    datapoints::Datapoint,
     primitives::{timestamp::VicInstant, Primitives},
-    topics::{TopicKey, TopicKeyHandle},
+    topics::{TopicKey, TopicKeyHandle, TopicKeyProvider},
 };
+use std::collections::{BTreeMap, BTreeSet};
+use thiserror::Error;
 #[derive(Debug, Clone)]
 pub struct Datastore {
     buckets: BTreeMap<TopicKeyHandle, BucketHandle>,
+}
+#[derive(Error, Debug)]
+pub enum DatastoreError {
+    #[error("Generic Datastore Error: {0}")]
+    Generic(String),
+    #[error("Bucket not found for topic {0}")]
+    BucketNotFound(TopicKey),
 }
 
 impl Datastore {
@@ -15,6 +23,23 @@ impl Datastore {
         Datastore {
             buckets: BTreeMap::new(),
         }
+    }
+
+    pub fn create_bucket<T: TopicKeyProvider>(&mut self, topic: &T) {
+        if !self.buckets.contains_key(&topic.handle()) {
+            let bucket = Bucket::new(topic.handle().clone());
+            self.buckets.insert(topic.handle().clone(), bucket);
+        }
+    }
+
+    pub fn get_bucket<T: TopicKeyProvider>(
+        &self,
+        topic: &T,
+    ) -> Result<BucketHandle, DatastoreError> {
+        self.buckets
+            .get(&topic.handle())
+            .cloned()
+            .ok_or_else(|| DatastoreError::BucketNotFound(topic.key().clone()))
     }
 
     pub fn add_primitive(&mut self, topic: TopicKey, time: VicInstant, value: Primitives) {
@@ -29,5 +54,67 @@ impl Datastore {
         let bucket = self.buckets.get_mut(&topic).unwrap();
 
         bucket.write().unwrap().add_primitive(time, value);
+    }
+
+    pub fn get_latest_primitive(&self, topic: TopicKey) -> Option<Primitives> {
+        let topic = topic.handle();
+        self.buckets
+            .get(&topic)
+            .and_then(|b| b.read().unwrap().get_latest_value().cloned())
+    }
+
+    pub fn get_latest_primitives(
+        &self,
+        topics: BTreeSet<TopicKey>,
+    ) -> BTreeMap<TopicKey, Primitives> {
+        topics
+            .iter()
+            .filter_map(|t| self.get_latest_primitive(t.clone()).map(|p| (t.clone(), p)))
+            .collect()
+    }
+
+    pub fn get_datapoints<T: TopicKeyProvider>(
+        &self,
+        topic: &T,
+    ) -> Result<Vec<Datapoint>, DatastoreError> {
+        let topic = topic.handle();
+        let bucket = self.get_bucket(&topic)?;
+        let bucket = bucket.read().unwrap();
+        Ok(bucket.get_datapoints())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::database::*;
+
+    #[test]
+    pub fn test_datastore_creation() {
+        let datastore = Datastore::new();
+        assert_eq!(datastore.buckets.len(), 0);
+    }
+
+    #[test]
+    pub fn test_datastore_create_bucket() {
+        let mut datastore = Datastore::new();
+        let topic = TopicKey::from_str("test/topic");
+        datastore.create_bucket(&topic);
+        assert_eq!(datastore.buckets.len(), 1);
+        assert!(datastore.buckets.contains_key(&topic.handle()));
+    }
+
+    #[test]
+    pub fn test_datastore_get_bucket() {
+        let mut datastore = Datastore::new();
+        let topic = TopicKey::from_str("test/topic");
+
+        let bucket_failed = datastore.get_bucket(&topic);
+        assert_eq!(bucket_failed.is_err(), true);
+
+        datastore.create_bucket(&topic);
+
+        let bucket = datastore.get_bucket(&topic);
+        assert_eq!(bucket.is_ok(), true);
+        assert_eq!(bucket.unwrap().read().unwrap().topic, topic);
     }
 }
