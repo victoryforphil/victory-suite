86caed96a4773aad1db9184ca71cc7e83cb4e8b2
Alex Carter <victoryforphil@gmail.com>
2024-09-28 19:02:03 -0700

    [Base] Added base files imported from datastore in victory-broker

---
+++ src/primitives/mod.rs
@@ -0,0 +1,23 @@
+use blob::VicBlob;
+
+use crate::{
+    time::{VicDuration, VicInstant},
+    topics::TopicIDType,
+};
+
+pub mod blob;
+pub mod integer;
+pub mod string;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Primitives {
+    Instant(VicInstant),
+    Duration(VicDuration),
+    Integer(i64),
+    Float(f64),
+    Text(String),
+    Blob(VicBlob),
+    Boolean(bool),
+    List(Vec<Primitives>),
+    Reference(TopicIDType),
+}
