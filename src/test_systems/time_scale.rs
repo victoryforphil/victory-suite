9cdd66a14e9725898426a3bd69499787f9b12403
Alex Carter <victoryforphil@gmail.com>
2024-10-16 21:53:53 -0700

    End to end Datastore + Command test

---
+++ src/test_systems/time_scale.rs
@@ -0,0 +1,49 @@
+use std::process::Output;
+
+use crate::system::System;
+use log::info;
+use serde::{Deserialize, Serialize};
+use victory_data_store::{database::DataView, topics::TopicKey};
+use victory_time_rs::Timespan;
+
+#[derive(Serialize, Deserialize)]
+pub struct TimeScaleData {
+    pub test_value: f32,
+}
+
+pub struct TimeScaleSystem {
+    pub topic_to_scale: TopicKey,
+}
+
+impl TimeScaleSystem {
+    pub fn new(sub_topic: &str) -> TimeScaleSystem {
+        TimeScaleSystem {
+            topic_to_scale: sub_topic.into(),
+        }
+    }
+}
+
+impl System for TimeScaleSystem {
+    fn init(&mut self) {}
+    fn cleanup(&mut self) {}
+
+    fn execute<'a>(&mut self, inputs_data: &'a DataView, dt: Timespan) -> DataView {
+        let inputs: TimeScaleData = inputs_data.get_latest(&self.topic_to_scale).unwrap();
+
+        let mut outputs = DataView::new();
+        let out_data = TimeScaleData {
+            test_value: inputs.test_value + dt.secs() as f32,
+        };
+
+        outputs.add_latest(&self.topic_to_scale.clone(), out_data);
+        outputs
+    }
+
+    fn get_subscribed_topics(
+        &self,
+    ) -> std::collections::BTreeSet<victory_data_store::topics::TopicKey> {
+        let mut topics = std::collections::BTreeSet::new();
+        topics.insert(self.topic_to_scale.clone());
+        topics
+    }
+}
