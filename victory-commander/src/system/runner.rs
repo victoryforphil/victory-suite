84826b103e844bd0c0bf749bcafa1b684551530b
Alex Carter <victoryforphil@gmail.com>
2024-10-20 21:40:12 -0700

    [Feature] Added realtime to runner

--- victory-commander/src/system/runner.rs
+++ victory-commander/src/system/runner.rs
@@ -11,6 +11,7 @@ pub struct BasherSysRunner {
     pub end_time: Timepoint,
     pub current_time: Timepoint,
     pub dt: Timespan,
+    pub real_time: bool,
 }
 
 impl BasherSysRunner {
@@ -21,13 +22,17 @@ impl BasherSysRunner {
             end_time: Timepoint::zero(),
             current_time: Timepoint::zero(),
             dt: Timespan::new_hz(100.0),
+            real_time: false,
         }
     }
 
     pub fn add_system(&mut self, system: SystemHandle) {
         self.systems.push(system);
     }
-
+    
+    pub fn set_real_time(&mut self, real_time: bool) {
+        self.real_time = real_time;
+    }
     pub fn run(&mut self, end_time: Timepoint) {
         self.end_time = end_time;
         self.current_time = Timepoint::zero();
@@ -38,6 +43,7 @@ impl BasherSysRunner {
         }
         info!("Running main loop for {:?}s", self.end_time.secs());
         while self.current_time < self.end_time {
+            let start_time = Timepoint::now();
             for system in self.systems.iter_mut() {
                 let mut system = system.lock().unwrap();
                 let sub = system.get_subscribed_topics();
@@ -50,6 +56,11 @@ impl BasherSysRunner {
            
                 let new_data = system.execute(&inputs, self.dt.clone());
                 self.data_store.apply_view(new_data).unwrap();
+            }
+            let end_time = Timepoint::now();
+            let elapsed = end_time - start_time;
+            if self.real_time {
+                std::thread::sleep(elapsed.as_duration());
             }
             self.current_time = self.current_time.clone() + self.dt.clone();
         }
