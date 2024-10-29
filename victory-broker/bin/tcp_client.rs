b48198eb49d5928c800f621ffe866cd6fc5a1638
Alex Carter <victoryforphil@gmail.com>
2024-10-29 00:29:39 -0700

    Removed tokio

--- victory-broker/bin/tcp_client.rs
+++ victory-broker/bin/tcp_client.rs
@@ -1,7 +1,5 @@
 use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-    time::Duration,
+    collections::HashMap, sync::{Arc, Mutex}, thread, time::Duration
 };
 
 use log::{debug, info};
@@ -12,15 +10,15 @@ use victory_broker::{
 use victory_data_store::{database::Datastore, topics::TopicKey};
 use victory_wtf::Timepoint;
 
-#[tokio::main]
-async fn main() {
+
+fn main() {
     pretty_env_logger::init();
-    let mut client = TCPClientAdapter::new(TCPClientOptions::from_url("0.0.0.0:7001")).await;
+    let mut client = TCPClientAdapter::new(TCPClientOptions::from_url("0.0.0.0:7001"));
 
     while client.is_err() {
         info!("Failed to connect to server, retrying...");
-        tokio::time::sleep(Duration::from_secs_f32(1.0)).await;
-        client = TCPClientAdapter::new(TCPClientOptions::from_url("0.0.0.0:7001")).await;
+        thread::sleep(Duration::from_secs_f32(1.0));
+        client = TCPClientAdapter::new(TCPClientOptions::from_url("0.0.0.0:7001"));
     }
     let client = client.unwrap();
 
@@ -31,7 +29,7 @@ async fn main() {
     let mut node = Node::new("TCP Client".to_string(), client_handle, datastore.clone());
     node.register();
     loop {
-        tokio::time::sleep(Duration::from_secs_f32(0.25)).await;
+        thread::sleep(Duration::from_secs_f32(0.25));
         node.tick();
         datastore.lock().unwrap().add_primitive(
             &topic_key,
